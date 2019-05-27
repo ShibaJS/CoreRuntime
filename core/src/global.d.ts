a8a536510517a856c7bd69e5db5d27152c59665f
Tlaster <tlaster@outlook.com>
2019-05-27 15:33:19 +0800

    update

--- core/src/global.d.ts
+++ core/src/global.d.ts
@@ -3,9 +3,9 @@
 // @ts-ignore
 declare global {
     // @ts-ignore
-    function registerComponent(name: string, view: IView, dataContext?: () => any): void;
+    function registerComponent(name: string, view: IView): void;
     // @ts-ignore
-    function runShibaApp(view: IView): void;
+    // function runShibaApp(view: IView): void;
     namespace JSX {
         // tslint:disable-next-line:interface-name
         interface IntrinsicElements {
--- template/webpack.config.js
+++ template/webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.tsx",
   mode: "production",
+  target: "node",
   module: {
     rules: [
       {
